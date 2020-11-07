bf3b84bbf282301aa1307650029b2bf74b56e714
Valentyna-Chudik <valentyna_chudik@ukr.net>
2020-11-08 01:18:24 +0200

    Fetch, templates added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import './styles.css';
+import countryCardTpl from './templates/country-card.hbs';
 import API from './js/fetchCountries.js';
+import getRefs from './js/get-refs.js';
 
+const refs = getRefs();
 
+refs.searchForm.addEventListener('input', onSearch);
+
+function onSearch(evt) {
+  evt.preventDefault();
+
+  const form = evt.currentTarget;
+  const searchQuery = form.elements.search.value;
+
+  API.fetchCountries(searchQuery)
+    .then(renderCountryCard)
+    .catch(onFetchError)
+    .finally(() => form.reset());
+  
+};
+
+function renderCountryCard(countryName) {
+    const markup = countryCardTpl(countryName);
+    refs.cardContainer.innerHTML = markup;
+};
+
+function onFetchError(error) {
+  alert('ERROR!!!');
+};
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,27 +1,7 @@
-// const BASE_URL = 'https://restcountries.eu/rest/v2/name';
-
-// export default function fetchCountries(countryName) {
-//   return fetch(`${BASE_URL}/${countryName}`).then(response =>
-//     response.json(),
-//   );
-// };
-
-import countryCardTpl from "../templates/country-card.hbs";
-
-const refs = {
-  cardContainer: document.querySelector('.js-card-container'), 
-}
-
-const r = fetch(`https://restcountries.eu/rest/v2/name/ukraine`)
-  .then(response => {
-  return response.json();
-})
-  .then(countryName => {
-    console.log(countryName);
-    const markup = countryCardTpl(countryName);
-    refs.cardContainer.innerHTML = markup;
-  })
-  .catch(error => {
-    console.log(error);
-  });
-
+const BASE_URL = `https://restcountries.eu/rest/v2`;
+ 
+function fetchCountries(countryName) {
+  return fetch(`${BASE_URL}/name/${countryName}`).then(response => response.json());
+};
+ 
+export default { fetchCountries };
